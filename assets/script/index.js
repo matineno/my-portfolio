7419407048c85f7bebee5e3120257249b043d51b
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-08-15 22:23:22 -0500

    used emailJs instead for message handling

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,3 +2,19 @@ function scrollToTarget() {
     var targetElement = document.getElementById("target");
     targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // These IDs are your EmailJS Service ID, Template ID, and your public key
+    const serviceID = 'service_ti9xi9n';
+    const templateID = 'template_rhlwjkd';
+
+    // Send the email
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            alert('Message sent!');
+        }, (err) => {
+            alert(JSON.stringify(err));
+        });
+});
